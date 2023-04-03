52e196d1f8db4153c320c95ef0797499fe52c1d1
joe <chries19921104@proton.me>
2023-04-03 08:28:47 +0800

    fix bug

--- app/adminapi/route/bet.php
+++ app/adminapi/route/bet.php
@@ -3,6 +3,6 @@
 use think\facade\Route;
 
 Route::group('bet', function () {
-    //投注订单列表
-    Route::get('order/list', 'v1.bet.order.Order/index')->option(['real_name' => '投注订单列表']);
+    //订单列表
+    Route::get('order/list', 'v1.bet.order.Order/index')->option(['real_name' => '订单列表']);
 });
